b67d616e880a3e2f4f8a82abc10bf27a5840d640
Anna Marie Sterling <sterling.am@gmail.com>
2022-08-30 17:09:40 -0600

    Item CRUD

--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -1,23 +1,67 @@
 require 'rails_helper'
 
 describe 'Items API' do
-  it 'can return a list of the merchants items' do
-    id = create(:merchant).id
+  it 'sends a list of items' do
     create_list(:item, 5)
 
-    get "/api/v1/merchants/#{id}/items"
+    get '/api/v1/items'
+
+    response_body = JSON.parse(response.body, symbolize_names: true)
+    items = response_body[:data]
 
     expect(response).to be_successful
 
+    items.each do |item|
+      expect(item).to have_key(:id)
+      expect(item[:id]).to be_a(String)
+
+      expect(item).to have_key(:attributes)
+      expect(item[:attributes][:name]).to be_a(String)
+      expect(item[:attributes][:description]).to be_a(String)
+      expect(item[:attributes][:unit_price]).to be_a(Float)
+      expect(item[:attributes][:merchant_id]).to be_an(Integer)
+      expect(item[:attributes]).to_not have_key(:created_at)
+    end
+  end
+
+  it 'can get one merchant by its ID' do
+    id = create(:item).id
+
+    get "/api/v1/items/#{id}"
+
     response_body = JSON.parse(response.body, symbolize_names: true)
-    merchant = response_body[:data]
+    item = response_body[:data]
 
-    expect(merchant).to have_key(:id)
-    expect(merchant[:id]).to be_a(String)
+    expect(response).to be_successful
 
-    expect(merchant).to have_key(:attributes)
-    expect(merchant[:attributes][:name]).to be_a(String)
+    expect(item).to have_key(:id)
+    expect(item[:id]).to be_a(String)
 
-    expect(merchant[:attributes]).to_not have_key(:created_at)
+    expect(item).to have_key(:attributes)
+    expect(item[:attributes][:name]).to be_a(String)
+    expect(item[:attributes][:description]).to be_a(String)
+    expect(item[:attributes][:unit_price]).to be_a(Float)
+    expect(item[:attributes][:merchant_id]).to be_an(Integer)
+    expect(item[:attributes]).to_not have_key(:created_at)
+  end
+
+  it "can create a new item" do
+    item_params = ({
+                    name: Faker::Commerce.product_name,
+                    description: Faker::Lorem.paragraph,
+                    unit_price: Faker::Number.decimal(l_digits: 2),
+                    merchant_id: create(:merchant).id
+                  })
+
+    headers = {"CONTENT_TYPE" => "application/json"}
+
+    post "/api/v1/items", headers: headers, params: JSON.generate(item: item_params)
+    created_item = Item.last
+
+    expect(response).to be_successful
+    expect(created_item.name).to eq(item_params[:name])
+    expect(created_item.description).to eq(item_params[:description])
+    expect(created_item.unit_price).to eq(item_params[:unit_price])
+    expect(created_item.merchant_id).to eq(item_params[:merchant_id])
   end
 end
